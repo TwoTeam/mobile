7646c39641827afdf6c3f64d46ed506e7a166686
WinterChilly <primoz.pesjak@gmail.com>
2015-07-14 19:19:41 +0200

    TutMaterialDesignV2.5
    Drawer 0.5

--- TutMaterialDesign/app/src/main/java/primoz/programmer/com/tutmaterialdesign/MainActivity.java
+++ TutMaterialDesign/app/src/main/java/primoz/programmer/com/tutmaterialdesign/MainActivity.java
@@ -1,6 +1,7 @@
 package primoz.programmer.com.tutmaterialdesign;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,8 +18,13 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         toolbar = (Toolbar) findViewById(R.id.app_bar);
-
         setSupportActionBar(toolbar);
+
+        NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+
+
     }
 
     @Override
@@ -41,7 +47,7 @@ public class MainActivity extends ActionBarActivity {
         }
 
         if (id == R.id.navigate) {
-            startActivity(new Intent(this,SubActivity.class));
+            startActivity(new Intent(this, SubActivity.class));
         }
 
         return super.onOptionsItemSelected(item);
