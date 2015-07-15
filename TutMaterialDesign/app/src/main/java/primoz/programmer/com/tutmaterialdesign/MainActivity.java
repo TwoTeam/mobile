7a3e1cd73b009a02a7bb7f24a8df46532b09eed0
WinterChilly <primoz.pesjak@gmail.com>
2015-07-15 11:56:58 +0200

    tutMaterialDesignV2.2
    NavigationDrawer se odpre, prazn

--- TutMaterialDesign/app/src/main/java/primoz/programmer/com/tutmaterialdesign/MainActivity.java
+++ TutMaterialDesign/app/src/main/java/primoz/programmer/com/tutmaterialdesign/MainActivity.java
@@ -20,9 +20,11 @@ public class MainActivity extends ActionBarActivity {
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         NavigationDrawerFragment drawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
-        drawerFragment.setUp((DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout),toolbar);
 
 
     }
